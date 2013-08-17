0038e1cc6de906a7f7be4f9be9ad8617210c59a7
Brian J. Cardiff <bcardiff@manas.com.ar>
2013-08-17 18:42:55 -0300

    fix type. add more tests.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -73,7 +73,7 @@ void assert_eq_fails_with_values_and_location() {
   ASSERT_EQ(e.location(), l);
 }
 
-void assert_eq_succed_if_values_match() {
+void assert_eq_succeed_if_values_match() {
   ASSERT_EQ(42, 42);
 }
 
@@ -147,6 +147,35 @@ void assert_eq_with_floats() {
   ASSERT_EQ(42.0f, 42.0f);
 }
 
+void assert_succeed_on_true() {
+  ASSERT(true);
+}
+
+void assert_fails_on_false() {
+  ValueExpectationException<string> e;
+  string l;
+  ASSERT_RAISE_A(ValueExpectationException<string>, e, {
+    l = BKMK; ASSERT(false);
+  });
+
+  ASSERT_EQ(e.location(), l);
+  ASSERT_EQ(e.actual(), "false");
+  ASSERT_EQ(e.expected(), "true");
+}
+
+int counter_f = 0;
+int f() { counter_f++; return 1; }
+int counter_g = 0;
+int g() { counter_g++; return 0; }
+void expressions_are_evaluated_once() {
+  ASSERT_RAISE({
+    ASSERT_EQ(f(), g())
+  });
+
+  ASSERT_EQ(counter_f, 1);
+  ASSERT_EQ(counter_g, 1);
+}
+
 int main() {
   RUN_TEST(assert_raise_should_not_raise_if_the_exception_was_expected);
   RUN_TEST(assert_raise_should_raise_if_a_missing_exception_was_expected);
@@ -157,13 +186,18 @@ int main() {
   RUN_TEST(assert_raise_a_should_raise_if_the_exception_was_of_wrong_type_with_inner_exception);
 
   RUN_TEST(assert_eq_fails_with_values_and_location);
-  RUN_TEST(assert_eq_succed_if_values_match);
+  RUN_TEST(assert_eq_succeed_if_values_match);
 
   RUN_TEST(assert_eq_with_strings);
   RUN_TEST(assert_eq_with_char_ptr);
   RUN_TEST(assert_eq_with_strings_and_char_ptr);
   RUN_TEST(assert_eq_with_doubles);
   RUN_TEST(assert_eq_with_floats);
+
+  RUN_TEST(assert_succeed_on_true);
+  RUN_TEST(assert_fails_on_false);
+
+  RUN_TEST(expressions_are_evaluated_once);
 
   return 0;
 }
